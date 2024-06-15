731fa3eeeee9fc4b5cfc3ebd058574a2f90bb8d4
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-06-15 17:02:16 +0200

    Add project with migrations

---
+++ src/PersonalRecord.Domain/Migrations/20240615061315_CreateTableMovementRecordItems.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalRecord.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableMovementRecordItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovementRecordItems",
+                columns: table => new
+                {
+                    MovementRecordID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MvrWeight = table.Column<float>(type: "REAL", nullable: false),
+                    MvrReps = table.Column<int>(type: "INTEGER", nullable: false),
+                    MvrDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MvrMovementID_FK = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovementRecordItems", x => x.MovementRecordID);
+                    table.ForeignKey(
+                        name: "FK_MovementRecordItems_MovementItems_MvrMovementID_FK",
+                        column: x => x.MvrMovementID_FK,
+                        principalTable: "MovementItems",
+                        principalColumn: "MovementID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovementRecordItems_MvrMovementID_FK",
+                table: "MovementRecordItems",
+                column: "MvrMovementID_FK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovementRecordItems");
+        }
+    }
+}
---
+++ src/PersonalRecord.Domain/Models/Entities/MovementRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonalRecord.Domain.Models.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class MovementRecord
+    {
+        [Key]
+        public Guid MovementRecordID { get; set; }
+
+        public float MvrWeight { get; set; }
+
+        public int MvrReps { get; set; }
+
+        public DateTime MvrDate { get; set; }
+
+        public Guid MvrMovementID_FK { get; set; }
+
+        [ForeignKey(nameof(MvrMovementID_FK))]
+        public Movement Movement { get; set; }
+    }
+}
