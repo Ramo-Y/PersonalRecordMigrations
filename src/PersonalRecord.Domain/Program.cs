aba0c346a2e28c3ca8f91e0eb4d1bcb18f686636
Ramazan Yilmaz <ramazan.yilmaz@merkle.com>
2024-10-17 22:21:51 +0200

    Remove Swashbuckle.AspNetCore

--- src/PersonalRecord.Domain/Program.cs
+++ src/PersonalRecord.Domain/Program.cs
@@ -7,19 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
