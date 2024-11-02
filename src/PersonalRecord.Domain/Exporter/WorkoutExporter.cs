3a2bf4c51285e35c8a2b4d661272cc1d4d40de55
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-11-02 13:05:22 +0100

    Export WorkoutToExerciseItems directly on Workout

--- src/PersonalRecord.Domain/Exporter/WorkoutExporter.cs
+++ src/PersonalRecord.Domain/Exporter/WorkoutExporter.cs
@@ -25,7 +25,31 @@
                     WorkoutID = HelenGuid,
                     WokName = "Helen",
                     WokHeader = "3 Rounds For Time",
-                    WokNotes = helenStringBuilder.ToString()
+                    WokNotes = helenStringBuilder.ToString(),
+                    WokWorkoutToExerciseItems =
+                    [
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("dc52c198-a867-4c92-b964-c6d28e359b44"),
+                            WteWorkoutID_FK = HelenGuid,
+                            WteExerciseID_FK = ExerciseExporter.RunGuid,
+                            WteExerciseDistance = 400
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("6250b22c-02e1-47a0-ab01-2bcaeea1cc73"),
+                            WteWorkoutID_FK = HelenGuid,
+                            WteExerciseID_FK = ExerciseExporter.KettlebellSwingsGuid,
+                            WteExerciseRepCount = 21
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("79f9e18e-9c75-4ad4-aa7a-6ef1ccfe84b6"),
+                            WteWorkoutID_FK = HelenGuid,
+                            WteExerciseID_FK = ExerciseExporter.PullUpsGuid,
+                            WteExerciseRepCount = 12
+                        }
+                    ]
                 },
             };
 
