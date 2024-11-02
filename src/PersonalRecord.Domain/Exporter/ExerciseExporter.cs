a8e5bf2669ac52c9db6274b1dfb361eebe26b600
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-11-02 22:52:41 +0100

    Add Diane workout

--- src/PersonalRecord.Domain/Exporter/ExerciseExporter.cs
+++ src/PersonalRecord.Domain/Exporter/ExerciseExporter.cs
@@ -9,8 +9,12 @@
         internal static Guid RunGuid = new("ea5a4f3b-7781-4901-9894-a7f3d376db70");
 
         internal static Guid KettlebellSwingsGuid = new("ddd7610c-c7a0-44fa-8ff9-6e77cca1d244");
-        
+
         internal static Guid PullUpsGuid = new("c6f50c62-8d9a-4367-944f-c7244b23366e");
+
+        internal static Guid DeadliftsGuid = new("2e28deb5-d9b9-4900-b11d-519d01d86b3e");
+
+        internal static Guid HandstandPushUpsGuid = new("7efdf00a-0428-40a6-afc8-96a3c2ccd4d8");
 
         public override async Task GenerateAndExportAsync()
         {
@@ -30,7 +34,17 @@
                 {
                     ExerciseID = PullUpsGuid,
                     ExrName = "Pull-Ups"
-                }
+                },
+                new()
+                {
+                    ExerciseID = DeadliftsGuid,
+                    ExrName = "Deadlifts"
+                },
+                new()
+                {
+                    ExerciseID = HandstandPushUpsGuid,
+                    ExrName = "Handstand Push-Ups"
+                },
             };
 
             await Export(list);
--- src/PersonalRecord.Domain/Exporter/WorkoutExporter.cs
+++ src/PersonalRecord.Domain/Exporter/WorkoutExporter.cs
@@ -7,7 +7,8 @@
 
     public class WorkoutExporter : ExporterBase<Workout>
     {
-        internal static Guid HelenGuid = new("96e57fc6-12b4-4389-b8c4-e4a4b6a83672");
+        private static Guid HelenGuid = new("96e57fc6-12b4-4389-b8c4-e4a4b6a83672");
+        private static Guid DianeGuid = new("1b8c1fcb-1c32-4416-a4ef-2471a9597c84");
 
         public override async Task GenerateAndExportAsync()
         {
@@ -17,6 +18,13 @@
             helenStringBuilder.AppendLine("– Intermediate: 11-14 minutes");
             helenStringBuilder.AppendLine("– Advanced: 9-10 minutes");
             helenStringBuilder.AppendLine("– Elite: <8 minutes");
+
+            var dianeStringBuilder = new StringBuilder();
+            dianeStringBuilder.AppendLine("Good Times for Diane:");
+            dianeStringBuilder.AppendLine("– Beginner: 10-14 minutes");
+            dianeStringBuilder.AppendLine("– Intermediate: 6-9 minutes");
+            dianeStringBuilder.AppendLine("– Advanced: 5-6 minutes");
+            dianeStringBuilder.AppendLine("– Elite: <4 minutes");
 
             var list = new List<Workout>
             {
@@ -48,6 +56,58 @@
                             WteWorkoutID_FK = HelenGuid,
                             WteExerciseID_FK = ExerciseExporter.PullUpsGuid,
                             WteExerciseRepCount = 12
+                        }
+                    ]
+                },
+                new()
+                {
+                    WorkoutID = DianeGuid,
+                    WokName = "Diane",
+                    WokHeader = "21-15-9 Rounds For Time",
+                    WokNotes = dianeStringBuilder.ToString(),
+                    WokWorkoutToExerciseItems =
+                    [
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("bc75eb4c-586a-45f0-a0cd-2428b14f28e7"),
+                            WteWorkoutID_FK = DianeGuid,
+                            WteExerciseID_FK = ExerciseExporter.DeadliftsGuid,
+                            WteExerciseRepCount = 21
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("86fe2993-c428-4131-a65e-f467449d2953"),
+                            WteWorkoutID_FK = DianeGuid,
+                            WteExerciseID_FK = ExerciseExporter.HandstandPushUpsGuid,
+                            WteExerciseRepCount = 21
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("54d9b35c-95a3-4799-880c-021ee76218de"),
+                            WteWorkoutID_FK = DianeGuid,
+                            WteExerciseID_FK = ExerciseExporter.DeadliftsGuid,
+                            WteExerciseRepCount = 15
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("5b3d2d12-62eb-4a8f-9fdd-89490c431d52"),
+                            WteWorkoutID_FK = DianeGuid,
+                            WteExerciseID_FK = ExerciseExporter.HandstandPushUpsGuid,
+                            WteExerciseRepCount = 15
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("298e14f4-86c3-4e37-bd29-75734a9714bf"),
+                            WteWorkoutID_FK = DianeGuid,
+                            WteExerciseID_FK = ExerciseExporter.DeadliftsGuid,
+                            WteExerciseRepCount = 9
+                        },
+                        new()
+                        {
+                            WorkoutToExerciseID = new Guid("4603e55c-ec51-4ba5-9483-b727eef79da0"),
+                            WteWorkoutID_FK = DianeGuid,
+                            WteExerciseID_FK = ExerciseExporter.HandstandPushUpsGuid,
+                            WteExerciseRepCount = 9
                         }
                     ]
                 },
